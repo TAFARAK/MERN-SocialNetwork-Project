7950dca3725be27946ea2500fa8f72d78918b49b
TAFARAK <mohammadtafarak@gmail.com>
2022-08-01 01:16:25 +0530

    First Commit-entire project loaded

---
+++ server/index.js
@@ -0,0 +1,47 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import router from './routes/auth.js';
+import './models/user.js';
+
+
+
+const app = express();
+const PORT = 5000;
+
+app.use(express.json())
+app.use(router);
+
+const customMiddleware = (req,res,next) =>{
+    console.log("middlawre Executed MTR");
+    next();
+}
+
+
+app.get("/home",(req,res)=>{
+    console.log("I AM FROM HOME PAGE MTR")  //HOME
+    res.send("I AM  HOME PAGE MTR")
+})
+
+app.get("/login",customMiddleware,(req,res)=>{
+    console.log("I AM FROM LOGIN PAGE MTR")   //LOGING
+    res.send("I AM  LOGIN PAGE MTR")
+})
+
+app.listen(PORT,()=>{
+    console.log("i am tafarak from server:",PORT)
+})
+
+
+app.use(bodyParser.json({limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://socialnetwork:socialnetwork@cluster0.eyd0d.mongodb.net/?retryWrites=true&w=majority';
+
+mongoose.connect(CONNECTION_URL).then(() => {
+    console.log("SERVER connect to via database RUNNING ON:",PORT);
+})
+
+
---
+++ server/routes/auth.js
@@ -0,0 +1,53 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import '../../server/models/user.js'
+import bcryptjs from "bcryptjs"
+
+const router = express.Router();
+const User = mongoose.model("User");
+
+router.get('/love',(req,res) => {
+    res.send("HELLO MTR I AM FROM ROUTER")
+
+});
+
+router.post('/sigup',(req,res)=>{
+    const {name, email, password}= req.body;
+    if(!name || !email || !password){
+        return res.status(422).json({error:"Please Add All Details"});
+    }
+    User.findOne({email:email})
+        .then( (savedUser)=>{
+            if (savedUser){
+                return res.status(422).json({error:"User already Exists with that email"});
+            }
+
+            bcryptjs.hash(password,12)
+                    .then(hashedpassword=>{
+
+                    
+                            const user = new User({
+                                name:name,
+                                email:email,
+                                password:hashedpassword
+                            })
+                            user.save()
+                                .then(user=>{
+                                    res.json({message:"User saved Successfully in database"})
+                                })
+                                .catch(err=>{
+                                    console.log(err)
+                                })
+                    })
+                
+        })
+        .catch(err=>{
+            console.log(err)
+        })           
+
+});
+
+export default router;
+
+
+
